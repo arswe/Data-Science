412d6354cb224353d741924da40f89aae5d6dbc1
arswe <abdurrahmansoftw@gmail.com>
2024-05-30 21:17:59 +0600

    ML Projects.

---
+++ artificial intelligence/final exam/pythonProject1/main.py
@@ -0,0 +1,25 @@
+class Node:
+
+    def __init__(self, a, b, z):
+        self.x = a
+        self.y = b
+        self.level = z
+
+    class BFS:
+        def __init__(self):
+            self.directions = 4
+            self.x_move = [1, -1, 0, 0]
+            self.y_move = [0, 0, 1, -1]
+            self.found = False
+            self.goal_level = 0
+            self.source = None
+            self.goal = None
+            self.N = 0
+            self.state = 0
+            self.init()
+
+        def __int__(self):
+            graph = [
+                [0, 0, 1, 0, 1],
+
+            ]
